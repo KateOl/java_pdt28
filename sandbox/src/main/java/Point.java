31e7d9231ebc2bbd489d9fc9bddc7accbc30e87e
KateOl <kates13@rambler.ru>
2016-12-30 16:33:07 +0100

    changes

--- sandbox/src/main/java/Point.java
+++ sandbox/src/main/java/Point.java
@@ -2,20 +2,40 @@
 
 public class Point {
 
-    public static double x1=10,y1=15;
-    public static double x2=15,y2=21.3;
-    public static double c=(x1-x2);
-    public static double d=(y1-y2);
-    public static double a; // points
-    public static double b;
+    private double x1,y1;
+    private double x2,y2;
+    private double c=(x1-x2);
+    private double d=(y1-y2);
+    private double a; // points
+    private double b;
 
+    public void setX1(double x1) {
+        this.x1 = x1;
+    }
 
+    public void setY1(double y1) {
+        this.y1 = y1;
+    }
+
+    public void setX2(double x2) {
+        this.x2 = x2;
+    }
+
+    public void setY2(double y2) {
+        this.y2 = y2;
+    }
 
     public static void main(String[] args) {
 
        Point p = new Point();
 
-        System.out.println(Math.sqrt(Math.pow(c, 2) + Math.pow(d, 2)));
+        p.setX1(25.3);
+        p.setX2(10);
+        p.setY1(15.2);
+        p.setY2(10.3);
+
+
+        System.out.println(Math.sqrt(Math.pow(p.c, 2) + Math.pow(p.d, 2)));
 
         } //main
 
